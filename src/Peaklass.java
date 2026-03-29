e80eb2c66d61adaf6b8f2688ae8294b8d4e6306a
mariaelisa <vassiljeva.mariaelisa@gmail.com>
2026-03-29 17:43:46 +0300

    done

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,9 +1,5 @@
 public class Peaklass {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-        // testing, successful
->>>>>>> a85ac96bece840dc84a0c843edc7aeab10ac3e12
     }
 }
