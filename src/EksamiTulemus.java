745bc437303233b026fd7f0a70b849b8fd3c99d5
Viktorija Korjagina <viktorija.korjagina@ut.ee>
2026-05-12 10:45:06 +0300

    Update EksamiTulemus.java

--- src/EksamiTulemus.java
+++ src/EksamiTulemus.java
@@ -1,1 +1,34 @@
+// See klass hoiab eksami lõpptulemuse andmeid
+// Siin ei arvutata midagi, vaid ainult salvestatakse juba arvutatud tulemus
+public class EksamiTulemus {
 
+    private int teadmised;
+    private int enesekindluseBoonus;
+    private int õnn;
+    private int lõplikSkoor;
+    private String tulemusTekst;
+
+    public EksamiTulemus(int teadmised, int enesekindluseBoonus, int õnn, int lõplikSkoor, String tulemusTekst) {
+        this.teadmised = teadmised;
+        this.enesekindluseBoonus = enesekindluseBoonus;
+        this.õnn = õnn;
+        this.lõplikSkoor = lõplikSkoor;
+        this.tulemusTekst = tulemusTekst;
+    }
+
+    public int getLõplikSkoor() {
+        return lõplikSkoor;
+    }
+    public String getTulemusTekst() {
+        return tulemusTekst;
+    }
+
+    // See meetod teeb eksami tulemusest pika teksti
+    public String getTäisTekst() {
+        return tulemusTekst + "\n\n" +
+                "Teadmised: " + teadmised + "\n" +
+                "Enesekindluse boonus: " + enesekindluseBoonus + "\n" +
+                "Õnnetegur: " + õnn + "\n\n" +
+                "Lõplik skoor: " + lõplikSkoor;
+    }
+}
