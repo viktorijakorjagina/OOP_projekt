490fe4480587755ba4d481e08b0d659f72a5a2dd
Viktorija Korjagina <viktorija.korjagina@ut.ee>
2026-05-12 10:39:16 +0300

    koodi parandused

--- src/TegevusteLooja.java
+++ src/TegevusteLooja.java
@@ -1,1 +1,47 @@
+// See klass loob kõik mängus kasutatavad tegevused
+// Eraldi klass on mugav, sest tegevuste nimekiri ei sega mängu juhtimise koodi
+public class TegevusteLooja {
+    
+    // Meetod tagastab tegevuste massiivi
+    public static Tegevus[] looTegevused() {
+        return new Tegevus[] {
 
+                // Tegevus(nimi, kirjeldus, minTeadmised, maxTeadmised, enesekindlusMuutus)
+                new Tegevus(
+                        "Õppimine",
+                        "Avad märkmikud ja õpid päriselt.",
+                        10,
+                        20,
+                        5
+                ),
+                new Tegevus(
+                        "Loengu vaatamine",
+                        "Vaatad loenguvideot ja loodad, et midagi jääb meelde.",
+                        0,
+                        10,
+                        10
+                ),
+                new Tegevus(
+                        "Ülesannete lahendamine",
+                        "Lahendad ülesandeid ja saad targemaks.",
+                        10,
+                        20,
+                        5
+                ),
+                new Tegevus(
+                        "Ainult üks episood",
+                        "Avad Netflixi ainult üheks episoodiks.",
+                        0,
+                        0,
+                        -5
+                ),
+                new Tegevus(
+                        "Puhkamine",
+                        "Otsustad puhata. Aju vajab pausi.",
+                        0,
+                        0,
+                        -5
+                )
+        };
+    }
+}
