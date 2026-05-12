7e883923b864100f0dcd9fd2959ec6b24faad336
Viktorija Korjagina <viktorija.korjagina@ut.ee>
2026-05-12 11:38:48 +0300

    koodi parandused

--- src/Eksam.java
+++ src/Eksam.java
@@ -1,26 +1,22 @@
 import javax.swing.JOptionPane;
 import java.util.Random;
 
-/**
- * Klass Eksam korraldab lõpliku eksami.
- * Tulemus sõltub teadmistest, enesekindlusest ja juhuslikust õnnetegurist.
- * Läbimiseks on vaja 50+ punkti.
- */
+// Klass Eksam korraldab lõpliku eksami
+// Tulemus sõltub teadmistest, enesekindlusest ja juhuslikust õnnetegurist
+// Läbimiseks on vaja 50+ punkti
 public class Eksam {
 
-    private static final int LÄBIMISE_PIIR = 50; // läbimiseks vajalik punktide arv
+    private static final int läbimisePiir = 50; // läbimiseks vajalik punktide arv
     private Random random = new Random();
-
-    /**
-     * Viib läbi lõpliku eksami ja näitab tulemust.
-     * @param õpilane mängija, kes sooritab eksami
-     */
+    
+    // Viib läbi lõpliku eksami ja näitab tulemust
+    // @param õpilane mängija, kes sooritab eksami
     public void sooritaEksam(Õpilane õpilane) {
 
         // Näita eksamieelset infot
         JOptionPane.showMessageDialog(null,
                 "EKSAMI PÄEV ON KÄES!\n\n" +
-                        "Sinu teadmiste ja enesekindluse punktid:\n" +
+                        "Sinu lõplikud statistikud:\n" +
                         õpilane.getStatsText(),
                 "Eksami päev!",
                 JOptionPane.INFORMATION_MESSAGE);
@@ -41,12 +37,12 @@ public class Eksam {
                         "Enesekindluse boonus: " + (enesekindlus / 5) + " punkti\n" +
                         "Õnnetegur: " + õnneTekst + " punkti\n\n" +
                         "LÕPLIK SKOOR: " + lõplikSkoor + " punkti\n" +
-                        "(Läbimiseks vajad: " + LÄBIMISE_PIIR + " pumkti)",
+                        "(Läbimiseks vajad: " + läbimisePiir + " pumkti)",
                 "Tulemused",
                 JOptionPane.INFORMATION_MESSAGE);
 
         // Näita lõpptulemust
-        if (lõplikSkoor >= LÄBIMISE_PIIR) {
+        if (lõplikSkoor >= läbimisePiir) {
             JOptionPane.showMessageDialog(null,
                     "SA LÄBISID EKSAMI!\n\n" + getLäbimiseSõnum(teadmised, õnn),
                     "Õnnitlused :)",
@@ -61,10 +57,10 @@ public class Eksam {
 
     // Tagastab läbimise sõnumi
     private String getLäbimiseSõnum(int teadmised, int õnn) {
-        if (teadmised >= LÄBIMISE_PIIR) {
+        if (teadmised >= läbimisePiir) {
             return "Selgub, et õppimine ei olegi scam.";
         }
-        else if (teadmised < LÄBIMISE_PIIR && õnn > 10) {
+        else if (õnn >= 15){
             return "Sa teadsid peaaegu mitte midagi, aga õnn oli sinu poolel.";
         }
         else {
@@ -77,7 +73,7 @@ public class Eksam {
         if (teadmised < 25) {
             return "Sa veetsid rohkem aega Netfliksis kui märkmetes.\nŠokeeriv tulemus. Tõeliselt šokeeriv.";
         }
-        else if (teadmised >= LÄBIMISE_PIIR) {
+        else if (teadmised >= läbimisePiir) {
             return "Kehv õnn. Sa proovisid päriselt, aga universum ütles ei.\nMitte sinu päev.";
         }
         else {
